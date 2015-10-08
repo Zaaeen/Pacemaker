3e4c9bf0a496743962197bcbdaaae7057eb36f2d
Jordan Loubser <zaaeen@gmail.com>
2015-10-08 12:11:32 +0100

    Fixed Tests

--- src/models/User.java
+++ src/models/User.java
@@ -34,10 +34,13 @@ public class User
 	@Override
 	public String toString()
 	{
-		return toStringHelper(this).addValue(firstName)
+		return toStringHelper(this)
+				.addValue(id)
+				.addValue(firstName)
 				.addValue(lastName)
 				.addValue(password)
-				.addValue(email)                               
+				.addValue(email)  
+				
 				.toString();
 	}
 }
